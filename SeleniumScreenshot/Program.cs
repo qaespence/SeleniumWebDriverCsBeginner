24b54ed38bef27a104b2685adbc1cfdb809bc60e
Eric Spence <ericspence@gmail.com>
2019-06-28 18:42:44 -0700

    adding Section 6: Taking Screenshots

---
+++ SeleniumScreenshot/Program.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+
+namespace SeleniumScreenshot
+{
+    class Program
+    {
+        static void Main()
+        {
+            IWebDriver chrome = new ChromeDriver(@"e:\CodeArea\");
+
+            string screenshotsDir = Directory.GetCurrentDirectory() + @"\screenshots";
+
+            chrome.Navigate().GoToUrl("http://google.com");
+            
+            Screenshot googleScreenshot = ((ITakesScreenshot)chrome).GetScreenshot();
+
+            if (Directory.Exists(screenshotsDir))
+            {
+                Directory.CreateDirectory(screenshotsDir);
+            }
+
+            googleScreenshot.SaveAsFile(screenshotsDir + @"\googlescreenshot.png", ScreenshotImageFormat.Png);
+
+            chrome.Quit();
+        }
+    }
+}
